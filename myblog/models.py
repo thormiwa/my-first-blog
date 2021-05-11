3705d6758729ed7a58bb243ba32ad849c175755d
thormiwa <thormiwa04@gmail.com>
2021-05-11 21:00:24 +0100

    test

--- myblog/models.py
+++ myblog/models.py
@@ -6,8 +6,8 @@ class Blog(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    date_created = models.DateField(default=timezone.now)
-    published_date = models.DateField(blank=True, null=True)
+    date_created = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now
