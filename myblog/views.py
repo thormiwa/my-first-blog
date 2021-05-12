7447b000944a61ce1700f4841cc54b1af25c567d
thormiwa <thormiwa04@gmail.com>
2021-05-12 11:46:52 +0100

    made changes to my blog

--- myblog/views.py
+++ myblog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Blog
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'myblog/post_list.html', {})
+    blogs = Blog.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'myblog/post_list.html', {'blogs': blogs})
