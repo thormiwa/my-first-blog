80d241f79308086c6df5f0f4b5eb5ad6ea044b81
thormiwa <thormiwa04@gmail.com>
2021-05-17 18:04:13 +0100

    adding changes

--- myblog/urls.py
+++ myblog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('blog/<int:pk>/', views.post_detail, name='post_detail'),
+    path('blog/new/', views.post_new, name='post_new'),
+    path('blog/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Blog
 from django.shortcuts import get_object_or_404
+from .forms import BlogForm
 
 # Create your views here.
 def post_list(request):
@@ -11,3 +13,34 @@ def post_list(request):
 def post_detail(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
     return render(request, 'myblog/post_detail.html', {'blog': blog})
+
+def post_new(request):
+    form = BlogForm()
+    return render(request, 'myblog/post_edit.html', {'form': form})
+
+def post_new(request):
+    if request.method == "POST":
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.author = request.user
+            blog.published_date = timezone.now()
+            blog.save()
+            return redirect('post_detail', pk=blog.pk)
+    else:
+        form = BlogForm()
+    return render(request, 'myblog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    if request.method == "POST":
+        form = BlogForm(request.POST, instance=blog)
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.author = request.user
+            blog.published_date = timezone.now()
+            blog.save()
+            return redirect('post_detail', pk=blog.pk)
+    else:
+        form = BlogForm(instance=blog)
+    return render(request, 'myblog/post_edit.html', {'form': form})
